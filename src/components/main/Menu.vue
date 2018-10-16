5894760d0325ea5c09f86e5629a9a211610936f6
agerarg <ager.arg@gmail.com>
2018-10-16 20:56:40 -0300

    update1

---
+++ src/components/main/Menu.vue
@@ -0,0 +1,40 @@
+<template>
+    	<nav id="menu">
+									<ul>
+										<router-link :to="{name: 'Default'}" tag="li"><a>Homepage</a></router-link>
+										<router-link :to="{name: 'Generic'}" tag="li"><a >Generic</a></router-link>
+									<router-link :to="{name: 'Elements'}" tag="li"><a >Elements</a></router-link>
+										<li>
+											<span class="opener">Submenu</span>
+											<ul>
+												<li><a href="#">Lorem Dolor</a></li>
+												<li><a href="#">Ipsum Adipiscing</a></li>
+												<li><a href="#">Tempus Magna</a></li>
+												<li><a href="#">Feugiat Veroeros</a></li>
+											</ul>
+										</li>
+										<li><a href="#">Etiam Dolore</a></li>
+										<li><a href="#">Adipiscing</a></li>
+										<li>
+											<span class="opener">Another Submenu</span>
+											<ul>
+												<li><a href="#">Lorem Dolor</a></li>
+												<li><a href="#">Ipsum Adipiscing</a></li>
+												<li><a href="#">Tempus Magna</a></li>
+												<li><a href="#">Feugiat Veroeros</a></li>
+											</ul>
+										</li>
+										<li><a href="#">Maximus Erat</a></li>
+										<li><a href="#">Sapien Mauris</a></li>
+										<li><a href="#">Amet Lacinia</a></li>
+									</ul>
+								</nav>
+</template>
+<style lang="scss" scoped>
+#menu{ 
+	position: fixed; 
+	width:  _size(sidebar-width);
+	background-color: green;
+}
+</style>
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,27 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Elements from '@/components/view/Elements.vue'
+import Default from '@/components/view/Default.vue'
+import Generic from '@/components/view/Generic.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
+      path: '/elements',
+      name: 'Elements',
+      component: Elements
+    },
+    {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Default',
+      component: Default
+    },
+    {
+      path: '/generic',
+      name: 'Generic',
+      component: Generic
     }
   ]
 })
